72966a68e47dbdcf85dd8ddfc77aee2827e78ec0
LuiFeAn <luisfernandogvv@gmail.com>
2024-01-13 01:24:57 -0300

    feat: Criando value object para a descrição

---
+++ src/application/entities/todos/value_objects/description.ts
@@ -0,0 +1,33 @@
+
+
+export class Description {
+
+    private readonly description: string
+
+    constructor(description: string){
+
+        if( this.validateDescriptionLenght(description) ){
+
+            throw new Error('The description must have a maximum of 5000 characters');
+
+        }
+
+        this.description = description;
+
+    }
+
+    get value(){
+
+        return this.description;
+
+    }
+
+    private validateDescriptionLenght(description: string){
+
+        return (
+            description.length > 5000
+        )
+
+    }
+
+}
